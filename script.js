84bf19310c7f89836735248eb956b671db761de5
Emerson465 <72088901+Emerson465@users.noreply.github.com>
2021-12-23 23:23:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+function countText() {
+  let text = document.form_main.text.value;
+  let qCarac = text.length;
+  document.getElementById('characters').innerText = qCarac;
+}
+
+function posAcao() {
+  document.getElementById("digite").innerText = "Frase limpinha :)";
+  document.getElementById("digite").style.color = "#2BDBA1";
+  document.getElementById("text").style.borderColor = "#2BDBA1"
+  document.getElementById("botao").style.background = "#2BDBA1";
+  document.getElementById("botao").innerText = "Limpar outra frase";
+}
+
+function limpar(){
+  let palavras = ["merda", "porra", "corno", "corna", "puto", "puta", "caralho", "desgraça", "imbecil", "idiota"];
+  let palavra = new RegExp(palavras.join("|") , "gi");
+  texto = document.getElementById("text").value;
+  resultado = texto.replace(palavra, "***");
+  document.getElementById("text").value = resultado;
+  console.log(resultado);
+}
+
+function acao(){
+  
+  if  (document.getElementById("botao").innerText == "Limpar outra frase"){
+    document.location.reload(true);
+  }
+  else{
+    limpar();
+  }
+  
+  
+
+
+  posAcao();
+}

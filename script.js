fb742f85b1b61468a460ecef597b91ec32528c7e
Emerson465 <72088901+Emerson465@users.noreply.github.com>
2021-12-27 19:20:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,31 +9,40 @@ function countText() {
 function posAcao() {
   document.getElementById("digite").innerText = "Frase limpinha :)";
   document.getElementById("digite").style.color = "#2BDBA1";
-  document.getElementById("text").style.borderColor = "#2BDBA1"
+  document.getElementById("text").style.borderColor = "#2BDBA1";
   document.getElementById("botao").style.background = "#2BDBA1";
   document.getElementById("botao").innerText = "Limpar outra frase";
 }
 
-function limpar(){
-  let palavras = ["merda", "porra", "corno", "corna", "puto", "puta", "caralho", "desgraça", "imbecil", "idiota", "viado"];
-  let palavra = new RegExp(palavras.join("|") , "gi");
+function limpar() {
+  let palavras = ["merda", "porra", "corno", "corna", "puto", "puta", "caralho", "desgraça", "imbecil", "idiota", "viado", "cu"];
+  let palavra = new RegExp(palavras.join("|"), "gi");
   texto = document.getElementById("text").value;
   resultado = texto.replace(palavra, "***");
   document.getElementById("text").value = resultado;
-  console.log(resultado);
 }
 
-function acao(){
-  
-  if  (document.getElementById("botao").innerText == "Limpar outra frase"){
+function countPalavroes() {
+  let palavras = ["merda", "porra", "corno", "corna", "puto", "puta", "caralho", "desgraça", "imbecil", "idiota", "viado", "cu"];
+  let palavra = new RegExp(palavras.join("|"), "gi");
+  var str = document.getElementById("text").value;
+  var count = (str.match(palavra) || []).length;
+  console.log(count);
+  document.getElementById("res").innerText = count + " palavrões ";
+  document.getElementById("countPalavroes").style.display = "block";
+}
+
+function acao() {
+
+  countPalavroes();
+
+  if (document.getElementById("botao").innerText === "Limpar outra frase") {
     document.location.reload(true);
   }
-  else{
+  else {
     limpar();
   }
-  
-  
-
 
   posAcao();
+
 }
